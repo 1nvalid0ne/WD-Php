f75cb8ce1d0e35235a14d72f7b59499928ae1ab7
1nvalid0ne <sterlingmccrevey11@protonmail.com>
2021-02-12 19:04:19 -0600

    copied files overeops

---
+++ alert.js
@@ -0,0 +1,3 @@
+let credits = "Song by: Blip Stream Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By Attribution 3.0 License"
+let copyright = " All images taken from google, no copyright intended."
+alert(credits + copyright);
---
+++ todo.md
@@ -0,0 +1,4 @@
+make a newsletter sign-up form
+make the music follow through each page
+make a version log
+organize css and comment everywhere
